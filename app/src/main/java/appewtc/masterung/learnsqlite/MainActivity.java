fe13f047367559ed91f924ffed01c318920eae26
Taweep <taweep.wut@gmail.com>
2017-01-07 15:00:10 +0700

    Finish 7 Jan

--- app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -1,7 +1,9 @@
 package appewtc.masterung.learnsqlite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bingWidget();
-
+        buttonController();
 
     }   // Main Method นี่คือเมธอดหลัก
+
+    private void buttonController() {
+        //for signUp
+        singUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent (MainActivity.this, SignUp.class));
+            }
+        });
+
+    } // buttonController
 
     //คือการผูกความสัมพันธ์ระหว่าง ตัวแปรและ object บน Activity
     private void bingWidget() {
 
+        singInButton = (Button) findViewById(R.id.button);
+        singUpButton = (Button) findViewById(R.id.button2);
 
     }
 
