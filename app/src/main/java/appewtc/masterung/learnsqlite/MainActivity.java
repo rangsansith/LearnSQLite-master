e8be2522d0bcc7c81030416926f6321aa6ba4d4d
Taweep <taweep.wut@gmail.com>
2017-01-07 14:00:51 +0700

    Try a

---
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -0,0 +1,18 @@
+package appewtc.masterung.learnsqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }   // Main Method นี่คือเมธอดหลัก
+
+}   // Main Class
