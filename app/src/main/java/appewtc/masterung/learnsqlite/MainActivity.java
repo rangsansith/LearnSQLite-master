7e98c6058e01e9bb9a57d6edca629219f064060d
Taweep <taweep.wut@gmail.com>
2017-01-07 14:15:43 +0700

    Bing Finish

--- app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -2,17 +2,28 @@ package appewtc.masterung.learnsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
-
+    private Button singInButton, singUpButton;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bingWidget();
+
+
     }   // Main Method นี่คือเมธอดหลัก
+
+    //คือการผูกความสัมพันธ์ระหว่าง ตัวแปรและ object บน Activity
+    private void bingWidget() {
+
+
+    }
 
 }   // Main Class
