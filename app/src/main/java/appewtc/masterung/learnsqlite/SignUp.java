bc355151119f67b75f4938fbfb1c478962b88e15
T.WUT <taweep.wut@gmail.com>
2017-01-08 10:17:16 +0700

    Start 8 Jan

--- app/src/main/java/appewtc/masterung/learnsqlite/SignUp.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/SignUp.java
@@ -2,12 +2,31 @@ package appewtc.masterung.learnsqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }  // Main Method
-}  // Main Class
+
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+        button = (Button) findViewById(R.id.button3);
+
+    }
+
+}   // Main Class
