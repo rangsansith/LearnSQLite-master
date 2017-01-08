25f4b89bd05873bc6f942f915601245dd8ad36ee
T.WUT <taweep.wut@gmail.com>
2017-01-08 13:53:04 +0700

    Check Space OK

--- app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
+++ app/src/main/java/appewtc/masterung/learnsqlite/MainActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private Button singInButton, singUpButton;
     private MyManage myManage;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
     @Override
@@ -34,14 +38,44 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent (MainActivity.this, SignUp.class));
             }
         });
+        // For SingIn
+        singInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkAuthen();
+
+            }
+        });
+
 
     } // buttonController
+
+    private void checkAuthen() {
+        // Get Value
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+            // Have Space
+            myDialog(getResources().getString(R.string.haveSpace));
+
+
+        }
+
+    } // checkAuthen
+
+    private void myDialog(String string) {
+        Toast.makeText(MainActivity.this,string, Toast.LENGTH_SHORT).show();
+
+    }
 
     //คือการผูกความสัมพันธ์ระหว่าง ตัวแปรและ object บน Activity
     private void bingWidget() {
 
         singInButton = (Button) findViewById(R.id.button);
         singUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
 
     }
 
